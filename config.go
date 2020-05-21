9f78c6c01af7c5922655e55707fb6b2b64c1d3d9
Alex <work@xingyongtao.xyz>
2020-05-21 10:25:47 +0800

    move MongoConf & MySQLConf to hs package, for easy use

--- config.go
+++ config.go
@@ -5,6 +5,18 @@ import (
 	"os"
 )
 
+type MongoConf struct {
+	URI         string `json:"uri"`
+	Database    string `json:"database"`
+	MaxPoolSize uint64 `json:"maxPoolSize"`
+	MinPoolSize uint64 `json:"minPoolSize"`
+	AppName     string `json:"appName"`
+}
+
+type MySQLConf struct {
+	URI string `json:"uri"`
+}
+
 func ParseJsonConfig(filename string, config interface{}) error {
 	configFile, err := os.Open(filename)
 	defer func() {
