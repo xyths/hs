794c9caf00c789b8a923ad8b8e2357cf9289820c
Alex <work@xingyongtao.xyz>
2020-07-16 21:34:47 +0800

    add broadcast.New

--- broadcast/broadcaster.go
+++ broadcast/broadcaster.go
@@ -1,16 +1,29 @@
 package broadcast
 
+import "fmt"
+
 type Broadcaster interface {
-	Broadcast(labels []string, direction, price, amount, total, profit string)
+	Broadcast(labels []string, symbol, direction, price, amount, total, profit string)
 }
 
 const (
 	Buy  = "buy"
 	Sell = "sell"
+
+	NameDingTalk = "dingtalk"
 )
 
 type Config struct {
 	Name    string
 	BaseUrl string `json:"baseUrl"`
 	Secret  string
+}
+
+func New(config Config) Broadcaster {
+	switch config.Name {
+	case NameDingTalk:
+		return NewDingTalk(config)
+	default:
+		panic(fmt.Sprintf("robot %s not supported", config.Name))
+	}
 }
