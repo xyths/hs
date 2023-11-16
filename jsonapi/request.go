814861a4092cf3018e891719395a2be3c0a4e567
Alex <39478565+xyths@users.noreply.github.com>
2023-11-16 17:18:15 +0800

    add json api GET with header

---
+++ jsonapi/request.go
@@ -0,0 +1,28 @@
+package jsonapi
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func GetWithHeader(ctx context.Context, url string, header map[string]string, response interface{}) error {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(body, response); err != nil {
+		return err
+	}
+	return nil
+}
