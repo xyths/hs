1333636a8c4503474fa513d44a4522b6c69c7f6a
Alex <39478565+xyths@users.noreply.github.com>
2024-01-03 15:07:25 +0800

    add PostWithForm

--- jsonapi/request.go
+++ jsonapi/request.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	urlpkg "net/url"
+	"strings"
 )
 
 func GetWithHeader(ctx context.Context, url string, header map[string]string, response interface{}) error {
@@ -15,6 +17,35 @@ func GetWithHeader(ctx context.Context, url string, header map[string]string, re
 	for k, v := range header {
 		req.Header.Add(k, v)
 	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(body, response); err != nil {
+		return err
+	}
+	return nil
+}
+
+func PostWithForm(ctx context.Context, url string, header, data map[string]string, response interface{}) error {
+	data_ := urlpkg.Values{}
+	for k, v := range data {
+		data_.Set(k, v)
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, strings.NewReader(data_.Encode()))
+	if err != nil {
+		return err
+	}
+	for k, v := range header {
+		req.Header.Add(k, v)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
